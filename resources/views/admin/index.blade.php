8b3a657f44822118c4b065c4a1b5daecaf26a548
Sebastian <berglingtim918@gmail.com>
2022-05-10 18:53:00 -0500

    Modificaciones al login etc...

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,40 +6,6 @@
 @stop
 
 @section('content')
-    <style>
-
-        .content-wrapper {
-            background: #ADA996;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #eaeaea56, #dbdbdb86, #f2f2f25d, #ada99660);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #eaeaea5d, #dbdbdb5b, #f2f2f260, #ada99650); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-            background-size: 100%;
-            background-position: 0px -47px;
-            background-repeat: no-repeat;
-
-
-        }
-        #card{
-            border-radius: 10px;
-            border: 7px;
-            border-bottom: black;
-            background: #ADA996;  /* fallback for old browsers */
-            background: -webkit-linear-gradient(to right, #eaeaea80, #dbdbdb8c, #f2f2f28c, #ada99688);  /* Chrome 10-25, Safari 5.1-6 */
-            background: linear-gradient(to right, #eaeaea83, #dbdbdb79, #f2f2f27c, #ada99675); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-            -webkit-box-shadow: -34px 21px 50px 22px rgba(0,0,0,0.91);
-            -moz-box-shadow: -34px 21px 50px 22px rgba(0,0,0,0.91);
-            box-shadow: -34px 21px 50px 22px rgba(0,0,0,0.91);
-        }
-        #texto{
-            text-shadow: -1px -1px 1px #aaa,0px 4px 1px rgba(0,0,0,0.5),4px 4px 5px rgba(0,0,0,0.7),0px 0px 7px rgba(0,0,0,0.4);
-            letter-spacing: 1px;
-            word-spacing: 2px;
-            font-family: fantasy;
-            font-size: 30px;
-        }
-    </style>
-    <br>
-
     <div class="card" id="card">
         <div class="card-body" >
                 <div class="row mb-3">
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,51 +3,6 @@
 @section('content')
 <br>
 <br>
-<div class="container">
-    <div class="card">
-        <div class="card-header"><h3 class="text-center">Login</h3></div>
-        <div class="card-body">
-            <form method="POST" action="{{ route('login') }}" >
-                @csrf
-                <div class="mb-4">
-                    <br>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }} " required autocomplete="email" autofocus placeholder="Correo">
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="mb-4">
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Contraseña">
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-                <div class="mb-12 form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    <label class="form-check-label" for="exampleCheck1">Recuérdame</label>
-                </div>
-                <div id="password_request">
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('¿Olvidaste tu contraseña?') }}
-                        </a>
-                    @endif
-                </div>
-            </form>
-            <button id="sesion" type="submit" class="btn btn-primary">
-                Iniciar sesión
-            </button>
-        </div>
-
-           
-
-      </div>
-</div>
-
 <style>
     .container{
 
@@ -65,20 +20,79 @@
     #password_request{
         text-align: end;
     }
-    #sesion{
-        width: 445px;
-        height: 65px;
 
-
-    }
     body{
-        background-size:100%;
-        background-image: url('https://yacaremotos.shop/assets/img/portfolio/carousel/1.png');
+        font-family:Georgia, 'Times New Roman', Times, serif;
+        background-position: center center;
+        background-attachment: fixed;
+        background-color: #66999;
+        background-size: cover;
+        background-image: url('vendor/adminlte/dist/img/Fondo_login.png');
         background-repeat: no-repeat;
     }
 
+    #email::placeholder{
+        font-family: "Font Awesome 6 Free";
+    }
+
+    #password::placeholder{
+        font: var(--fa-font-solid);
+        content: "\f007";
+    }
+
+    #sesion{
+        height: 66px;
+        font-size: 20px;
+    }
 </style>
+<div class="container">
+    <div class="card">
+        <form method="POST" action="{{ route('login') }}" >
+            <div class="card-header"><h3 class="text-center" id="login">Iniciar sesión</h3></div>
+            <div class="card-body">
+                @csrf
+                <div class="mb-4">
+                    <br>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }} " required autocomplete="email" autofocus placeholder=" Correo">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder=" Contrasena">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="mb-12 form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="exampleCheck1">Recuérdame</label>
+                </div>
+                <div id="password_request">
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                        </a>
+                    @endif
+                </div>
+            </div>
+            <div class="card-footer-fluid">
+                <button id="sesion" type="submit" class="btn btn-primary form-control">
+                    Iniciar sesión
+                    &nbsp;
+                    <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                    </svg>
 
-
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 @endsection
