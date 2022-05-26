5af17b508a2e4bdf581c41783adaa290fd4024f2
Sebastian <berglingtim918@gmail.com>
2022-05-26 17:48:48 -0500

    Más bugs .__.

--- resources/views/proveedore/index.blade.php
+++ resources/views/proveedore/index.blade.php
@@ -22,7 +22,7 @@
                 <h3>Proveedores</h3>
             </span>
             @can('Proveedores_guardar')
-            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalproveedores" data-bs-whatever="@mdo">Agregar
+            <button type="button" class="btn btn-success" data-bs-toggle="modal"  data-bs-target="#modalproveedores" data-bs-whatever="@mdo">Agregar
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle-dotted" viewBox="0 0 16 16">
                     <path d="M8 0c-.176 0-.35.006-.523.017l.064.998a7.117 7.117 0 0 1 .918 0l.064-.998A8.113 8.113 0 0 0 8 0zM6.44.152c-.346.069-.684.16-1.012.27l.321.948c.287-.098.582-.177.884-.237L6.44.153zm4.132.271a7.946 7.946 0 0 0-1.011-.27l-.194.98c.302.06.597.14.884.237l.321-.947zm1.873.925a8 8 0 0 0-.906-.524l-.443.896c.275.136.54.29.793.459l.556-.831zM4.46.824c-.314.155-.616.33-.905.524l.556.83a7.07 7.07 0 0 1 .793-.458L4.46.824zM2.725 1.985c-.262.23-.51.478-.74.74l.752.66c.202-.23.418-.446.648-.648l-.66-.752zm11.29.74a8.058 8.058 0 0 0-.74-.74l-.66.752c.23.202.447.418.648.648l.752-.66zm1.161 1.735a7.98 7.98 0 0 0-.524-.905l-.83.556c.169.253.322.518.458.793l.896-.443zM1.348 3.555c-.194.289-.37.591-.524.906l.896.443c.136-.275.29-.54.459-.793l-.831-.556zM.423 5.428a7.945 7.945 0 0 0-.27 1.011l.98.194c.06-.302.14-.597.237-.884l-.947-.321zM15.848 6.44a7.943 7.943 0 0 0-.27-1.012l-.948.321c.098.287.177.582.237.884l.98-.194zM.017 7.477a8.113 8.113 0 0 0 0 1.046l.998-.064a7.117 7.117 0 0 1 0-.918l-.998-.064zM16 8a8.1 8.1 0 0 0-.017-.523l-.998.064a7.11 7.11 0 0 1 0 .918l.998.064A8.1 8.1 0 0 0 16 8zM.152 9.56c.069.346.16.684.27 1.012l.948-.321a6.944 6.944 0 0 1-.237-.884l-.98.194zm15.425 1.012c.112-.328.202-.666.27-1.011l-.98-.194c-.06.302-.14.597-.237.884l.947.321zM.824 11.54a8 8 0 0 0 .524.905l.83-.556a6.999 6.999 0 0 1-.458-.793l-.896.443zm13.828.905c.194-.289.37-.591.524-.906l-.896-.443c-.136.275-.29.54-.459.793l.831.556zm-12.667.83c.23.262.478.51.74.74l.66-.752a7.047 7.047 0 0 1-.648-.648l-.752.66zm11.29.74c.262-.23.51-.478.74-.74l-.752-.66c-.201.23-.418.447-.648.648l.66.752zm-1.735 1.161c.314-.155.616-.33.905-.524l-.556-.83a7.07 7.07 0 0 1-.793.458l.443.896zm-7.985-.524c.289.194.591.37.906.524l.443-.896a6.998 6.998 0 0 1-.793-.459l-.556.831zm1.873.925c.328.112.666.202 1.011.27l.194-.98a6.953 6.953 0 0 1-.884-.237l-.321.947zm4.132.271a7.944 7.944 0 0 0 1.012-.27l-.321-.948a6.954 6.954 0 0 1-.884.237l.194.98zm-2.083.135a8.1 8.1 0 0 0 1.046 0l-.064-.998a7.11 7.11 0 0 1-.918 0l-.064.998zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                 </svg>
@@ -56,17 +56,17 @@
                             </div>
                             <div class="mb-3">
                                 <label for="message-text" class="col-form-label">Documento <small style="color:red;">*</small></label>
-                                <input type="number" name="Documento_proveedor" class="form-control" id="recipient-name" value="{{old('Documento_proveedor')}}">
+                                <input type="number" name="Documento_proveedor" class="form-control" id="recipient-name" value="">
                                 <small class="text-danger">{{$errors->first('Documento_proveedor')}}</small>
                             </div>
                             <div class="mb-3">
                                 <label for="message-text" class="col-form-label">Nombre <small style="color:red;">*</small></label>
-                                <input type="text" name="Nombre_proveedor" class="form-control" id="recipient-name" value="{{old('Nombre_proveedor')}}">
+                                <input type="text" name="Nombre_proveedor" class="form-control" id="recipient-name" value="">
                                 <small class="text-danger">{{$errors->first('Nombre_proveedor')}}</small>
                             </div>
                             <div class="mb-3">
                                 <label for="message-text" class="col-form-label">Teléfono <small style="color:red;">*</small></label>
-                                <input type="number" name="Telefono_proveedor" class="form-control" id="recipient-name" value="{{old('Telefono_proveedor')}}">
+                                <input type="number" name="Telefono_proveedor" class="form-control" id="recipient-name" value="">
                                 <small class="text-danger">{{$errors->first('Telefono_proveedor')}}</small>
                             </div>
                             <div>
@@ -82,7 +82,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="message-text" class="col-form-label">Dirección <small style="color:red;">*</small></label>
-                                <input type="text" name="Direccion_proveedor" class="form-control" id="recipient-name" value="{{old('Direccion_proveedor')}}"></input>
+                                <input type="text" name="Direccion_proveedor" class="form-control" id="recipient-name" value="">
                                 <small class="text-danger">{{$errors->first('Direccion_proveedor')}}</small>
                             </div>
                             <div class="modal-footer">
@@ -176,11 +176,11 @@
                                 </td>
                             </tr>
                             <!-- modal editar -->
-                            <div class="modal fade" id="editar{{$proveedore->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="editar{{$proveedore->id}}" tabindex="-1" aria-labelledby="editar" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Editar Proveedores</h5>
+                                            <h5 class="modal-title" id="editar">Editar Proveedores</h5>
                                             <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
                                         </div>
                                     <div class="modal-body">
@@ -252,16 +252,17 @@
                                                 <small class="text-danger">{{$errors->first('Direccion_proveedor')}}</small>
                                             </div>
                                             <div class="modal-footer">
-                                                <a href="" type="button" id="cerrar" class="btn btn-danger" data-bs-dismiss="modal">Cancelar
+                                                <a type="button"  class="btn btn-danger" href="javascript:location.reload()">
+                                                    Cancelar
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
                                                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                                     </svg>
                                                 </a>
-                                                <button type="submit" class="btn btn-success">Agregar
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle-dotted" viewBox="0 0 16 16">
-                                                    <path d="M8 0c-.176 0-.35.006-.523.017l.064.998a7.117 7.117 0 0 1 .918 0l.064-.998A8.113 8.113 0 0 0 8 0zM6.44.152c-.346.069-.684.16-1.012.27l.321.948c.287-.098.582-.177.884-.237L6.44.153zm4.132.271a7.946 7.946 0 0 0-1.011-.27l-.194.98c.302.06.597.14.884.237l.321-.947zm1.873.925a8 8 0 0 0-.906-.524l-.443.896c.275.136.54.29.793.459l.556-.831zM4.46.824c-.314.155-.616.33-.905.524l.556.83a7.07 7.07 0 0 1 .793-.458L4.46.824zM2.725 1.985c-.262.23-.51.478-.74.74l.752.66c.202-.23.418-.446.648-.648l-.66-.752zm11.29.74a8.058 8.058 0 0 0-.74-.74l-.66.752c.23.202.447.418.648.648l.752-.66zm1.161 1.735a7.98 7.98 0 0 0-.524-.905l-.83.556c.169.253.322.518.458.793l.896-.443zM1.348 3.555c-.194.289-.37.591-.524.906l.896.443c.136-.275.29-.54.459-.793l-.831-.556zM.423 5.428a7.945 7.945 0 0 0-.27 1.011l.98.194c.06-.302.14-.597.237-.884l-.947-.321zM15.848 6.44a7.943 7.943 0 0 0-.27-1.012l-.948.321c.098.287.177.582.237.884l.98-.194zM.017 7.477a8.113 8.113 0 0 0 0 1.046l.998-.064a7.117 7.117 0 0 1 0-.918l-.998-.064zM16 8a8.1 8.1 0 0 0-.017-.523l-.998.064a7.11 7.11 0 0 1 0 .918l.998.064A8.1 8.1 0 0 0 16 8zM.152 9.56c.069.346.16.684.27 1.012l.948-.321a6.944 6.944 0 0 1-.237-.884l-.98.194zm15.425 1.012c.112-.328.202-.666.27-1.011l-.98-.194c-.06.302-.14.597-.237.884l.947.321zM.824 11.54a8 8 0 0 0 .524.905l.83-.556a6.999 6.999 0 0 1-.458-.793l-.896.443zm13.828.905c.194-.289.37-.591.524-.906l-.896-.443c-.136.275-.29.54-.459.793l.831.556zm-12.667.83c.23.262.478.51.74.74l.66-.752a7.047 7.047 0 0 1-.648-.648l-.752.66zm11.29.74c.262-.23.51-.478.74-.74l-.752-.66c-.201.23-.418.447-.648.648l.66.752zm-1.735 1.161c.314-.155.616-.33.905-.524l-.556-.83a7.07 7.07 0 0 1-.793.458l.443.896zm-7.985-.524c.289.194.591.37.906.524l.443-.896a6.998 6.998 0 0 1-.793-.459l-.556.831zm1.873.925c.328.112.666.202 1.011.27l.194-.98a6.953 6.953 0 0 1-.884-.237l-.321.947zm4.132.271a7.944 7.944 0 0 0 1.012-.27l-.321-.948a6.954 6.954 0 0 1-.884.237l.194.98zm-2.083.135a8.1 8.1 0 0 0 1.046 0l-.064-.998a7.11 7.11 0 0 1-.918 0l-.064.998zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
-                                                </svg>
+                                                <button type="submit" class="btn btn-success">Editar
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle-dotted" viewBox="0 0 16 16">
+                                                        <path d="M8 0c-.176 0-.35.006-.523.017l.064.998a7.117 7.117 0 0 1 .918 0l.064-.998A8.113 8.113 0 0 0 8 0zM6.44.152c-.346.069-.684.16-1.012.27l.321.948c.287-.098.582-.177.884-.237L6.44.153zm4.132.271a7.946 7.946 0 0 0-1.011-.27l-.194.98c.302.06.597.14.884.237l.321-.947zm1.873.925a8 8 0 0 0-.906-.524l-.443.896c.275.136.54.29.793.459l.556-.831zM4.46.824c-.314.155-.616.33-.905.524l.556.83a7.07 7.07 0 0 1 .793-.458L4.46.824zM2.725 1.985c-.262.23-.51.478-.74.74l.752.66c.202-.23.418-.446.648-.648l-.66-.752zm11.29.74a8.058 8.058 0 0 0-.74-.74l-.66.752c.23.202.447.418.648.648l.752-.66zm1.161 1.735a7.98 7.98 0 0 0-.524-.905l-.83.556c.169.253.322.518.458.793l.896-.443zM1.348 3.555c-.194.289-.37.591-.524.906l.896.443c.136-.275.29-.54.459-.793l-.831-.556zM.423 5.428a7.945 7.945 0 0 0-.27 1.011l.98.194c.06-.302.14-.597.237-.884l-.947-.321zM15.848 6.44a7.943 7.943 0 0 0-.27-1.012l-.948.321c.098.287.177.582.237.884l.98-.194zM.017 7.477a8.113 8.113 0 0 0 0 1.046l.998-.064a7.117 7.117 0 0 1 0-.918l-.998-.064zM16 8a8.1 8.1 0 0 0-.017-.523l-.998.064a7.11 7.11 0 0 1 0 .918l.998.064A8.1 8.1 0 0 0 16 8zM.152 9.56c.069.346.16.684.27 1.012l.948-.321a6.944 6.944 0 0 1-.237-.884l-.98.194zm15.425 1.012c.112-.328.202-.666.27-1.011l-.98-.194c-.06.302-.14.597-.237.884l.947.321zM.824 11.54a8 8 0 0 0 .524.905l.83-.556a6.999 6.999 0 0 1-.458-.793l-.896.443zm13.828.905c.194-.289.37-.591.524-.906l-.896-.443c-.136.275-.29.54-.459.793l.831.556zm-12.667.83c.23.262.478.51.74.74l.66-.752a7.047 7.047 0 0 1-.648-.648l-.752.66zm11.29.74c.262-.23.51-.478.74-.74l-.752-.66c-.201.23-.418.447-.648.648l.66.752zm-1.735 1.161c.314-.155.616-.33.905-.524l-.556-.83a7.07 7.07 0 0 1-.793.458l.443.896zm-7.985-.524c.289.194.591.37.906.524l.443-.896a6.998 6.998 0 0 1-.793-.459l-.556.831zm1.873.925c.328.112.666.202 1.011.27l.194-.98a6.953 6.953 0 0 1-.884-.237l-.321.947zm4.132.271a7.944 7.944 0 0 0 1.012-.27l-.321-.948a6.954 6.954 0 0 1-.884.237l.194.98zm-2.083.135a8.1 8.1 0 0 0 1.046 0l-.064-.998a7.11 7.11 0 0 1-.918 0l-.064.998zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                                                    </svg>
                                                 </button>
                                             </div>
                                         </form>
@@ -271,6 +272,18 @@
                             </div>
                             <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
                             <!-- END MODAL EDITAR -->
+                            <?php $id = $proveedore->id;  ?>
+                            <?php if($errors->any()){ ?>
+                                <script>
+
+                                    var id = JSON.parse(`<?php echo $id;  ?>`)
+                                    $(document).ready(function(){
+                                        $('#editar'+id).modal('show');
+                                        $('#modalproveedores').remove();
+
+                                    })
+                                </script>
+                            <?php } ?>
                         @endforeach
                     </tbody>
                 </table>
@@ -294,17 +307,19 @@
     </div>
 </div>
 
-<script>
-    document.addEventListener('keydown', function(event){
-        if(event.key === "Escape"){
-            cerrar();
-        }
-    });
-    $(document).ready(function(){
-        $('#cerrar').click(function(){
-            cerrar();
+    <script>
+        document.addEventListener('keydown', function(event){
+            if(event.key === "Escape"){
+                cerrar();
+            }
+        });
+        $(document).ready(function(){
+            $('#cerrar').click(function(){
+                cerrar();
 
             });
+
+
         });
     </script>
     <script>
@@ -363,7 +378,7 @@
         </script>
     <?php } ?>
 
-     <?php if($message = Session::get('success')){ ?>
+    <?php if($message = Session::get('success')){ ?>
         <script>
         Swal.fire({
             position: 'top-center',
@@ -376,7 +391,7 @@
         </script>
 
 
-<?php } ?>
+    <?php } ?>
     <?php if($errors->any()){ ?>
         <script>
             $(document).ready(function(){
@@ -384,4 +399,10 @@
             })
         </script>
     <?php } ?>
+    {{-- Script para evitar cierre de modal al dar click --}}
+    <script>
+        $(document).ready(function(){
+            $('#modalproveedores').modal({backdrop: 'static', keyboard: false})
+        })
+    </script>
 @endsection
