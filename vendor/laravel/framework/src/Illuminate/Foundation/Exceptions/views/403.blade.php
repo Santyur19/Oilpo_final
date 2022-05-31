35ea01bedba7388d8aff9dc65bca800444a41ee7
Cbernalmonterrosa <cbernalmonterrosa@gmail.com>
2022-05-31 08:46:41 -0500

    Se arregla vista agregar proveedor

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/403.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/403.blade.php
@@ -1,27 +1,5 @@
-@extends('errors::illustrated-layout')
+@extends('errors::minimal')
 
-@section('title', __('No autorizado'))
+@section('title', __('Forbidden'))
 @section('code', '403')
-
-@section('image')
-<div class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-    <img id="image" src="http://assets.stickpng.com/images/585f89e6cb11b227491c354c.png" alt="" srcset="">
-</div>
-<style>
-    body{
-        background-color: rgb(45, 143, 255);
-    }
-    #image{
-        width: 80%;
-        position: absolute;
-    }
-</style>
-@endsection
-
-@section('message', __($exception->getMessage() ?: 'No autorizado'))
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
+@section('message', __($exception->getMessage() ?: 'Forbidden'))
--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
@@ -1,12 +1,5 @@
-@extends('errors::illustrated-layout')
+@extends('errors::minimal')
 
+@section('title', __('Not Found'))
 @section('code', '404')
-
-@section('title', __('Página no encontrada'))
-@section('image')
-<div style="background-image: url('https://thumbs.dreamstime.com/b/mec%C3%A1nico-de-perro-con-levas-un-coches-sostiene-los-relojes-autos-fondo-blanco-aislado-197439787.jpg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-
-</div>
-@endsection
-
-@section('message', __('Lo sentimos, no se ha podido encontrar la página que buscas.'))
+@section('message', __('Not Found'))
