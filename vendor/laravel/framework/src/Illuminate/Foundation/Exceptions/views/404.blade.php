9e6f989f41be5cffba5c9eccd68c8c0cb01dcd32
sebastianAvicii <92390254+sebastianAvicii@users.noreply.github.com>
2022-06-02 10:30:38 -0500

    Update 404.blade.php

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
@@ -4,9 +4,17 @@
 
 @section('title', __('Página no encontrada'))
 @section('image')
-<div style="background-image: url('https://thumbs.dreamstime.com/b/mec%C3%A1nico-de-perro-con-levas-un-coches-sostiene-los-relojes-autos-fondo-blanco-aislado-197439787.jpg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+
+<div style="background-image: url('https://cdn.discordapp.com/attachments/881318396128526336/981636496467578960/perro_mec6.png');  width: 85%; background-position: 10% -90%;" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
 
 </div>
+<style>
+    body{
+        background: #8e9eab;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #eef2f3, #8e9eab);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #eef2f3, #8e9eab); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
+</style>
 @endsection
 
 @section('message', __('Lo sentimos, no se ha podido encontrar la página que buscas.'))
