b04280278a878428a3ff0a53c1663416e58ec77b
unknown <Sena@MEDAPRCSGFSD712.FormacionSena.RED>
2022-06-09 08:46:31 -0500

    Se arregla homecontroller

--- resources/views/ventas/index.blade.php
+++ resources/views/ventas/index.blade.php
@@ -88,6 +88,7 @@
                                                     <form class="boton" action="{{ route('Editar_estado_ventas') }}" method="POST">
                                                         @csrf @method('PUT')
                                                         <?php if($ventas->estado=="Activo") { ?>
+                                                            <input type="text" value="<?php echo $ventas->Factura ?>" name="id" hidden>
                                                             <button class="btn btn-success" type="submit" ><?php echo $ventas->estado ?>
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
                                                                     <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
